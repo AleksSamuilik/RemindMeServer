6f17799b0055792c1403469586030d30dae718bd
Spawn <alkspawn@gmail.com>
2019-11-14 22:28:53 +0300

    connect db and create entity

--- src/main/java/it/alex/remindme/server/config/DatabaseConfig.java
+++ src/main/java/it/alex/remindme/server/config/DatabaseConfig.java
@@ -7,9 +7,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
@@ -27,16 +31,34 @@ public class DatabaseConfig {
     @Resource
     private Environment environment;
 
-
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
         entityManager.setDataSource(dataSource());
         entityManager.setPackagesToScan(environment.getRequiredProperty("db.entity.package"));
         entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
         entityManager.setJpaProperties(getHibernateProperties());
 
-
         return entityManager;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(environment.getRequiredProperty("db.url"));
+        dataSource.setDriverClassName(environment.getRequiredProperty("db.driver"));
+        dataSource.setUsername(environment.getRequiredProperty("db.username"));
+        dataSource.setPassword(environment.getRequiredProperty("db.password"));
+
+        dataSource.setInitialSize(Integer.parseInt(environment.getRequiredProperty("db.initialSize")));
+        dataSource.setMinIdle(Integer.parseInt(environment.getRequiredProperty("db.minIdle")));
+        dataSource.setMaxIdle(Integer.parseInt(environment.getRequiredProperty("db.maxIdle")));
+        dataSource.setTimeBetweenEvictionRunsMillis(Long.parseLong(environment.getRequiredProperty("db.timeBeetweenEvictionRunsMillis")));
+        dataSource.setMinEvictableIdleTimeMillis(Long.parseLong(environment.getRequiredProperty("db.minEvictableIdleTimeMillis")));
+        dataSource.setTestOnBorrow(Boolean.valueOf(environment.getRequiredProperty("db.testOnBorrow")));
+        dataSource.setValidationQuery(environment.getRequiredProperty("db.validationQuery"));
+
+        return dataSource;
     }
 
     private Properties getHibernateProperties() {
@@ -46,18 +68,18 @@ public class DatabaseConfig {
             properties.load(is);
 
             return properties;
-        } catch (IOException e){
-            throw  new IllegalArgumentException("Can't find 'hibernate.properties' in classpath",e);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Can't find 'hibernate.properties' in classpath", e);
         }
     }
 
     @Bean
-    public DataSource dataSource(){
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUrl(environment.getRequiredProperty("db.url"));
-        dataSource.setDriverClassName(environment.getProperty("db.driver"));
-        dataSource.setUsername(environment.getProperty("db.username"));
-        dataSource.setUsername(environment.getProperty("db.password"));
-        return dataSource;
+    public PlatformTransactionManager platformTransactionManager() {
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+        return manager;
     }
+
+
 }
